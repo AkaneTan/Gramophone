acb25d3623588d62e412c0f9cb6190a9ff270b2d
nift4 <nift4@protonmail.com>
2023-09-13 18:10:10 +0200

    fix animations

--- app/src/main/java/org/akanework/gramophone/ui/fragments/BaseFragment.kt
+++ app/src/main/java/org/akanework/gramophone/ui/fragments/BaseFragment.kt
@@ -1,9 +1,11 @@
 package org.akanework.gramophone.ui.fragments
 
 import android.os.Bundle
+import android.view.View
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import androidx.fragment.app.Fragment
+import com.google.android.material.color.MaterialColors
 import com.google.android.material.transition.MaterialSharedAxis
 
 abstract class BaseFragment : Fragment() {
@@ -16,4 +18,10 @@ abstract class BaseFragment : Fragment() {
 		reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, /* forward= */ false)
 	}
 
+	// https://github.com/material-components/material-components-android/issues/1984#issuecomment-1089710991
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		val colorBackground = MaterialColors.getColor(view, android.R.attr.colorBackground)
+		view.setBackgroundColor(colorBackground)
+	}
 }
