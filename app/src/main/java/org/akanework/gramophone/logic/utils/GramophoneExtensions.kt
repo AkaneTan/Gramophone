b2828c0b3642f296cb96baf58416c1ee0281980e
nift4 <nift4@protonmail.com>
2023-09-17 16:47:46 +0200

    fix build

--- app/src/main/java/org/akanework/gramophone/logic/utils/GramophoneExtensions.kt
+++ app/src/main/java/org/akanework/gramophone/logic/utils/GramophoneExtensions.kt
@@ -3,6 +3,7 @@ package org.akanework.gramophone.logic.utils
 import android.app.Activity
 import android.content.Context
 import android.content.res.Resources
+import android.net.Uri
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
@@ -11,6 +12,7 @@ import android.view.ViewTreeObserver
 import android.view.inputmethod.InputMethodManager
 import android.widget.OverScroller
 import androidx.customview.widget.ViewDragHelper
+import androidx.media3.common.MediaItem
 import androidx.media3.session.MediaController
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import java.lang.reflect.Field
@@ -26,6 +28,10 @@ fun MediaController.playOrPause() {
 	} else {
 		play()
 	}
+}
+
+fun MediaItem.getUri(): Uri {
+	return localConfiguration!!.uri
 }
 
 fun Activity.closeKeyboard() {
