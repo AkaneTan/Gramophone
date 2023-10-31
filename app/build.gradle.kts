a744502bf86efb92f1dfc963ec88be79b46f4eed
Akane Beneckendorff <akane@akanework.org>
2023-10-31 09:12:11 +0800

    Gramophone: Bump version to `1.0.2`
    Signed-off-by: Akane Beneckendorff <akane@akanework.org>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "org.akanework.gramophone"
         minSdk = 21
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1" + "." + "git rev-parse --short=6 HEAD".runCommand(workingDir = rootDir)
+        versionCode = 3
+        versionName = "1.0.2" + "." + "git rev-parse --short=6 HEAD".runCommand(workingDir = rootDir)
         setProperty("archivesBaseName", "Gramophone-$versionName")
     }
 
