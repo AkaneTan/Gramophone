3fac603b4af74f12385d44899b662721970eec22
Akane Beneckendorff <akane@akanework.org>
2023-11-23 02:27:50 +0800

    ColorUtils: Make light background less boring
    Signed-off-by: Akane Beneckendorff <akane@akanework.org>

--- app/src/main/kotlin/org/akanework/gramophone/logic/utils/ColorUtils.kt
+++ app/src/main/kotlin/org/akanework/gramophone/logic/utils/ColorUtils.kt
@@ -27,13 +27,14 @@ object ColorUtils {
 
     enum class ColorType(
         var chroma: Float = 0f,
+        var chromaDark: Float = 0f,
         var lighting: Float = 0f,
         var lightingDark: Float = 0f
     ) {
-        COLOR_BACKGROUND_ELEVATED(1.2f, 0.99f, 0.99f),
-        COLOR_BACKGROUND(0.9f, 1.015f, 1.015f),
-        TOOLBAR_ELEVATED(0.6f, 0.97f, 1.5f),
-        COLOR_PRIMARY_FAINTED(0.4f, 0.8f, 0.8f)
+        COLOR_BACKGROUND_ELEVATED(1.2f, 1.2f, 0.99f, 0.99f),
+        COLOR_BACKGROUND(1.3f, 0.9f, 1.015f, 1.015f),
+        TOOLBAR_ELEVATED(0.6f, 0.6f, 0.97f, 1.5f),
+        COLOR_PRIMARY_FAINTED(0.4f, 0.4f,0.8f, 0.8f)
     }
 
     private fun manipulateHsl(
@@ -45,16 +46,17 @@ object ColorUtils {
         val hsl = FloatArray(3)
         ColorUtils.colorToHSL(color, hsl)
 
-        hsl[1] *= colorType.chroma
-        hsl[1] = min(hsl[1], 1f)
-
         if (isDarkMode(context)) {
             hsl[2] *= colorType.lightingDark
             hsl[2] = min(hsl[2], 1f)
+            hsl[1] *= colorType.chromaDark
+            hsl[1] = min(hsl[1], 1f)
             if (overrideAmoledColor) {
                 hsl[2] = 0f
             }
         } else {
+            hsl[1] *= colorType.chroma
+            hsl[1] = min(hsl[1], 1f)
             hsl[2] *= colorType.lighting
             hsl[2] = min(hsl[2], 1f)
         }
