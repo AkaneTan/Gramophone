94a2a22b00125d37fb30a4beab503f336f7862c9
Akane Beneckendorff <akane@akanework.org>
2023-11-22 23:17:44 +0800

    Gramophone: Update copyright info
    Signed-off-by: Akane Beneckendorff <akane@akanework.org>

--- app/src/main/kotlin/org/akanework/gramophone/logic/GramophoneGlideModule.kt
+++ app/src/main/kotlin/org/akanework/gramophone/logic/GramophoneGlideModule.kt
@@ -1,3 +1,20 @@
+/*
+ *     Copyright (C) 2023  Akane Foundation
+ *
+ *     Gramophone is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Gramophone is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.akanework.gramophone.logic
 
 import android.content.Context
--- app/src/main/kotlin/org/akanework/gramophone/ui/adapters/Sorter.kt
+++ app/src/main/kotlin/org/akanework/gramophone/ui/adapters/Sorter.kt
@@ -1,7 +1,24 @@
+/*
+ *     Copyright (C) 2023  Akane Foundation
+ *
+ *     Gramophone is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Gramophone is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.akanework.gramophone.ui.adapters
 
 import android.net.Uri
-import org.akanework.gramophone.logic.calculation.SupportComparator
+import org.akanework.gramophone.logic.comparators.SupportComparator
 
 class Sorter<T>(
     val sortingHelper: Helper<T>,
