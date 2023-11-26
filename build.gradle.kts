746ec2f001d5e20c4b85bcbbfc4b97b66f7be3f5
Akane Beneckendorff <akane@akanework.org>
2023-11-26 08:35:11 +0800

    Gramophone: Remove library dependency
    Signed-off-by: Akane Beneckendorff <akane@akanework.org>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,4 @@ plugins {
     id("com.android.application") version "8.2.0-rc03" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
     id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
-    id("com.android.library") version "8.2.0-rc03" apply false
 }
