469fa801fb5fc976dce91aa93bde3e7fc2031abc
Akane Beneckendorff <akane@akanework.org>
2023-11-30 02:56:58 +0800

    Gramophone: Update dependencies
    Signed-off-by: Akane Beneckendorff <akane@akanework.org>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,8 +86,8 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.activity:activity-ktx:1.8.1")
-    implementation("androidx.transition:transition-ktx:1.5.0-alpha04")
-    implementation("androidx.fragment:fragment-ktx:1.7.0-alpha06")
+    implementation("androidx.transition:transition-ktx:1.5.0-alpha05")
+    implementation("androidx.fragment:fragment-ktx:1.7.0-alpha07")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0-rc01")
     implementation("androidx.appcompat:appcompat:1.7.0-alpha03")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0-alpha13")
