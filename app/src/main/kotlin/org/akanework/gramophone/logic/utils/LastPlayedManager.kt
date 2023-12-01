48c5fc2252c1156ba01601e8d5f82e18c4a44174
Akane Beneckendorff <akane@akanework.org>
2023-12-02 03:01:35 +0800

    LastPlayedManager: Prevent recording month outbound
    * I have no idea why this is happening
    
    Signed-off-by: Akane Beneckendorff <akane@akanework.org>

--- app/src/main/kotlin/org/akanework/gramophone/logic/utils/LastPlayedManager.kt
+++ app/src/main/kotlin/org/akanework/gramophone/logic/utils/LastPlayedManager.kt
@@ -131,7 +131,13 @@ class LastPlayedManager(context: Context, private val mediaSession: MediaSession
                     val composer = b.readStringSafe()
                     val genre = b.readStringSafe()
                     val recordingDay = b.readInt()
-                    val recordingMonth = b.readInt()
+                    val recordingMonth =
+                        try {
+                            b.readInt()
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                            0
+                        }
                     val artistId = b.readLong()
                     val albumId = b.readLong()
                     val genreId = b.readLong()
