b473535d82d53e8705e59138d7abe565629c36a3
Kotonoha Akane <akane@akanework.org>
2024-01-03 14:16:39 +0800

    Gramophone: Update AGP version to `8.2.0`
    Signed-off-by: Kotonoha Akane <akane@akanework.org>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.0-rc03" apply false
+    id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.21" apply false
     id("com.google.devtools.ksp") version "1.9.21-1.0.+" apply false
     id("org.jetbrains.kotlin.plugin.parcelize") version "1.9.21" apply false
