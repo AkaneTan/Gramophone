53d1065d63d53b59a4842316c64100d18b403bd8
Kotonoha Akane <akane@akanework.org>
2024-01-22 14:55:58 +0800

    Translation: Fix derp
    Signed-off-by: Kotonoha Akane <akane@akanework.org>

--- app/src/main/kotlin/org/akanework/gramophone/logic/comparators/AlphaNumericComparator.kt
+++ app/src/main/kotlin/org/akanework/gramophone/logic/comparators/AlphaNumericComparator.kt
@@ -1,6 +1,6 @@
 /*
  *     Copyright (C) 2016  Farbod Safaei
- *                   2024  Akane Foundation
+ *                   2024 Akane Foundation
  *
  *     Gramophone is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
--- app/src/main/kotlin/org/akanework/gramophone/ui/adapters/BaseAdapter.kt
+++ app/src/main/kotlin/org/akanework/gramophone/ui/adapters/BaseAdapter.kt
@@ -1,5 +1,5 @@
 /*
- *     Copyright (C) 2024  Akane Foundation
+ *     Copyright (C) 2024 Akane Foundation
  *
  *     Gramophone is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
