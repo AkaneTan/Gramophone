f3ad1ac4f6c4f16d64d41e0cc65e0964bdabac07
nift4 <nift4@protonmail.com>
2024-04-06 12:56:26 +0200

    1.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         // keep support for it for a while.
         minSdk = 21
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.0.4.1"
+        versionCode = 7
+        versionName = "1.0.5"
         if (releaseType != "Release") {
             versionNameSuffix = myVersionName
         }
