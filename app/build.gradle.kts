f5a33baa8dfbc5499d70335b4c5d01367f622f80
Light summer <93428659+lightsummer233@users.noreply.github.com>
2024-04-06 16:41:16 +0800

    Adjust preference styles, update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ import com.android.build.gradle.tasks.PackageAndroidArtifact
 import org.jetbrains.kotlin.util.removeSuffixIfPresent
 import java.util.Properties
 
-val aboutLibsVersion = "11.1.0" // keep in sync with plugin version
+val aboutLibsVersion = "11.1.1" // keep in sync with plugin version
 
 plugins {
     id("com.android.application")
@@ -165,11 +165,11 @@ aboutLibraries {
 dependencies {
     val glideVersion = "5.0.0-rc01"
     val media3Version = "1.3.0"
-    implementation("androidx.core:core-ktx:1.13.0-beta01")
+    implementation("androidx.core:core-ktx:1.13.0-rc01")
     implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("androidx.concurrent:concurrent-futures-ktx:1.1.0")
-    implementation("androidx.transition:transition-ktx:1.5.0-beta01") // <-- for predictive back
-    implementation("androidx.fragment:fragment-ktx:1.7.0-beta01")
+    implementation("androidx.transition:transition-ktx:1.5.0-rc01") // <-- for predictive back
+    implementation("androidx.fragment:fragment-ktx:1.8.0-alpha01")
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.appcompat:appcompat:1.7.0-alpha03")
@@ -179,7 +179,7 @@ dependencies {
     implementation("androidx.media3:media3-session:$media3Version")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("com.github.bumptech.glide:glide:$glideVersion")
-    implementation("com.google.android.material:material:1.12.0-beta01")
+    implementation("com.google.android.material:material:1.12.0-rc01")
     implementation("me.zhanghai.android.fastscroll:library:1.3.0")
     implementation("com.mikepenz:aboutlibraries:$aboutLibsVersion")
     ksp("com.github.bumptech.glide:ksp:$glideVersion")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.3.0-rc02" apply false
-    val kotlinVersion = "2.0.0-Beta4"
+    id("com.android.application") version "8.3.1" apply false
+    val kotlinVersion = "2.0.0-Beta5"
     id("org.jetbrains.kotlin.android") version kotlinVersion apply false
-    id("com.google.devtools.ksp") version "$kotlinVersion-1.0.17" apply false
+    id("com.google.devtools.ksp") version "$kotlinVersion-1.0.20" apply false
     id("org.jetbrains.kotlin.plugin.parcelize") version kotlinVersion apply false
-    id("com.mikepenz.aboutlibraries.plugin") version "11.1.0" apply false
+    id("com.mikepenz.aboutlibraries.plugin") version "11.1.1" apply false
 }
 
 tasks.withType(JavaCompile::class.java) {
