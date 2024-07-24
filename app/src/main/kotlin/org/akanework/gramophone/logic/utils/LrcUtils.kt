78ed537219dccf0cde078d767006475225932355
AkaneTan <dev@akane.uk>
2024-07-24 21:41:31 +0800

    Revert "Merge pull request #229 from nicholasmd3/improve_64c042e"
    This reverts commit 6b59084a0dbde365260b69fd196ec0324308b05a, reversing
    changes made to 7504580985fc5756c5b8781e86aeba5de5648581.

--- app/src/main/kotlin/org/akanework/gramophone/logic/utils/LrcUtils.kt
+++ app/src/main/kotlin/org/akanework/gramophone/logic/utils/LrcUtils.kt
@@ -130,7 +130,19 @@ object LrcUtils {
 
         markTranslations(lyricsList)
 
-        lyricsList.removeAll { it.content.isEmpty() }
+        if (lyricsList.isNotEmpty()) {
+            var count = 0
+            while (true) {
+                if (count < lyricsList.size
+                    && lyricsList[count].content.isEmpty()) {
+                    lyricsList.removeAt(count)
+                    count --
+                } else {
+                    break
+                }
+                count++
+            }
+        }
 
         if (lyricsList.isEmpty() && lrcContent.isNotEmpty()) {
             lyricsList.add(MediaStoreUtils.Lyric(null, lrcContent, false))
