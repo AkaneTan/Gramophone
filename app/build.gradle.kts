0fc89695d120cc257433d2af55a7e7db6d9ccbbc
nift4 <nift4@protonmail.com>
2024-09-10 23:21:52 +0200

    bump version for gplay

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,8 +95,8 @@ android {
         // keep support for it for a while.
         minSdk = 21
         targetSdk = 34
-        versionCode = 12
-        versionName = "1.0.10"
+        versionCode = 13
+        versionName = "1.0.11"
         if (releaseType != "Release") {
             versionNameSuffix = myVersionName
         }
