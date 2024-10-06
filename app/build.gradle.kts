47e4a6820240b631716e3872976583e2cc450cd0
nift4 <nift4@protonmail.com>
2024-10-06 23:38:49 +0200

    Release 1.0.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,8 +104,8 @@ android {
         // keep support for it for a while.
         minSdk = 21
         targetSdk = 34
-        versionCode = 14
-        versionName = "1.0.12"
+        versionCode = 15
+        versionName = "1.0.13"
         if (releaseType != "Release") {
             versionNameSuffix = myVersionName
         }
