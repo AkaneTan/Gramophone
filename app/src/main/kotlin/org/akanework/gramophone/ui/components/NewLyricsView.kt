e60e0f6355b9c51a7ee6acaed305f0a12461c6db
nift4 <nift4@protonmail.com>
2024-10-20 20:29:56 +0200

    First round of theming

--- app/src/main/kotlin/org/akanework/gramophone/ui/components/NewLyricsView.kt
+++ app/src/main/kotlin/org/akanework/gramophone/ui/components/NewLyricsView.kt
@@ -11,33 +11,30 @@ import android.text.TextPaint
 import android.text.style.ForegroundColorSpan
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.graphics.TypefaceCompat
 import androidx.core.widget.NestedScrollView
+import androidx.preference.PreferenceManager
 import org.akanework.gramophone.R
+import org.akanework.gramophone.logic.dpToPx
+import org.akanework.gramophone.logic.getBooleanStrict
 import org.akanework.gramophone.logic.ui.spans.StaticLayoutBuilderCompat
 import org.akanework.gramophone.logic.utils.SemanticLyrics
 import org.akanework.gramophone.logic.utils.SpeakerEntity
 import org.akanework.gramophone.ui.MainActivity
 
-/*
-== CHECKLIST ==
-- horizontal scroll (check:D)
-- auto-scroll of lyrics
-- text that scales up and changes color when currently active
-- left/right alignment for vocal1/vocal2
-- colors? for wakaloke ext
-- small text for background / translation
-- even smaller text for a background translation?
-- individual highlighting of words
-- begin next line while current isn't complete yet (because that's just how songs work)
- */
+// TODO colors for wakaloke ext
+// TODO react to clicks
+// TODO animations
 class NewLyricsView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
+	private val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+	private val smallSizeFactor = 0.97f
 	private val defaultTextPaint = TextPaint().apply { color = Color.RED }
 	private val translationTextPaint = TextPaint().apply { color = Color.GREEN }
 	private val translationBackgroundTextPaint = TextPaint().apply { color = Color.BLUE }
 	private var wordActiveSpan: ForegroundColorSpan? = null
-	private var spForRender: List<Pair<StaticLayout, SpannableStringBuilder>>? = null
-	private var spForMeasure: Pair<Pair<Int, Int>, List<Pair<StaticLayout, SpannableStringBuilder>>>? = null
+	private var spForRender: List<SbItem>? = null
+	private var spForMeasure: Pair<Pair<Int, Int>, List<SbItem>>? = null
 	private var defaultTextColor = 0
 	private var highlightTextColor = 0
 	private var lyrics: SemanticLyrics? = null
@@ -46,13 +43,14 @@ class NewLyricsView(context: Context, attrs: AttributeSet) : View(context, attrs
 		get() = context as MainActivity
 	private val instance
 		get() = activity.getPlayer()
-	private val scrollView
+	private val scrollView // TODO autoscroll
 		get() = parent as NestedScrollView
 
 	init {
 		defaultTextPaint.textSize = context.resources.getDimension(R.dimen.lyric_text_size)
 		translationTextPaint.textSize = context.resources.getDimension(R.dimen.lyric_tl_text_size)
 		translationBackgroundTextPaint.textSize = context.resources.getDimension(R.dimen.lyric_tl_bg_text_size)
+		applyTypefaces()
 	}
 
 	fun updateTextColor(newColor: Int, newHighlightColor: Int) {
@@ -83,6 +81,21 @@ class NewLyricsView(context: Context, attrs: AttributeSet) : View(context, attrs
 	}
 
 	fun onPrefsChanged() {
+		applyTypefaces()
+		spForRender = null
+		spForMeasure = null
+		requestLayout()
+	}
+
+	private fun applyTypefaces() {
+		val typeface = if (prefs.getBooleanStrict("lyric_bold", false)) {
+			TypefaceCompat.create(context, null, 700, false)
+		} else {
+			TypefaceCompat.create(context, null, 500, false)
+		}
+		defaultTextPaint.typeface = typeface
+		translationTextPaint.typeface = typeface
+		translationBackgroundTextPaint.typeface = typeface
 	}
 
 	override fun onDraw(canvas: Canvas) {
@@ -102,40 +115,44 @@ class NewLyricsView(context: Context, attrs: AttributeSet) : View(context, attrs
 					lastWord.last > posForRender || currentLineStarted &&
 					lines.find { it.lyric.start > lines[i].lyric.start
 							&& posForRender > it.lyric.start } == null
+			if (!highlight)
+				canvas.translate(0f, it.paddingTop.toFloat())
 			if (highlight) {
+				canvas.translate(0f, it.paddingTop.toFloat() / smallSizeFactor)
 				canvas.save()
-				canvas.scale(1.25f, 1.25f)
+				canvas.scale(1f / smallSizeFactor, 1f / smallSizeFactor)
 				if (wordActiveSpan == null)
 					wordActiveSpan = ForegroundColorSpan(highlightTextColor)
 				if (lines != null && lines[i].lyric.words != null) {
 					val word = lines[i].lyric.words?.findLast { it.timeRange.start <= posForRender }
 					if (word != null) {
-						it.second.setSpan(wordActiveSpan, 0, word.charRange.last,
+						it.text.setSpan(wordActiveSpan, 0, word.charRange.last,
 							Spanned.SPAN_INCLUSIVE_INCLUSIVE)
 						setSpan = true
 					}
 				} else {
-					it.second.setSpan(wordActiveSpan, 0, it.second.length,
+					it.text.setSpan(wordActiveSpan, 0, it.text.length,
 						Spanned.SPAN_INCLUSIVE_INCLUSIVE)
 					setSpan = true
 				}
-			} else if (it.first.alignment != Layout.Alignment.ALIGN_NORMAL) {
+			} else if (it.layout.alignment != Layout.Alignment.ALIGN_NORMAL) {
 				canvas.save()
-				if (it.first.alignment == Layout.Alignment.ALIGN_OPPOSITE)
-					canvas.translate(width * 0.2f, 0f)
+				if (it.layout.alignment == Layout.Alignment.ALIGN_OPPOSITE)
+					canvas.translate(width * (1 - smallSizeFactor), 0f)
 				else
-					canvas.translate(width * 0.1f, 0f)
+					canvas.translate(width * ((1 - smallSizeFactor) / 2), 0f)
 			}
-			it.first.draw(canvas)
+			it.layout.draw(canvas)
+			val th = it.layout.height.toFloat() + it.paddingBottom
 			if (highlight) {
 				if (setSpan)
-					it.second.removeSpan(wordActiveSpan)
+					it.text.removeSpan(wordActiveSpan)
 				canvas.restore()
-				canvas.translate(0f, it.first.height.toFloat() * 1.25f)
+				canvas.translate(0f, th / smallSizeFactor)
 			} else {
-				if (it.first.alignment != Layout.Alignment.ALIGN_NORMAL)
+				if (it.layout.alignment != Layout.Alignment.ALIGN_NORMAL)
 					canvas.restore()
-				canvas.translate(0f, it.first.height.toFloat())
+				canvas.translate(0f, th)
 			}
 		}
 		canvas.restore()
@@ -155,28 +172,30 @@ class NewLyricsView(context: Context, attrs: AttributeSet) : View(context, attrs
 		spForRender = spForMeasure!!.second
 	}
 
-	override fun onAttachedToWindow() {
-		super.onAttachedToWindow()
-	}
-
-	fun buildSpForMeasure(lyrics: SemanticLyrics?, width: Int): Pair<Pair<Int, Int>,
-			List<Pair<StaticLayout, SpannableStringBuilder>>> {
-		val lines = lyrics?.unsyncedText ?: listOf("NO_LYRIC_FOUND")
+	fun buildSpForMeasure(lyrics: SemanticLyrics?, width: Int): Pair<Pair<Int, Int>, List<SbItem>> {
+		val lines = lyrics?.unsyncedText ?: listOf("NO_LYRIC_FOUND") // TODO NO_LYRIC_FOUND
 		val syncedLines = (lyrics as? SemanticLyrics.SyncedLyrics?)?.text
 		val spLines = lines.mapIndexed { i, it ->
 			val sb = SpannableStringBuilder(it)
-			val align = if (syncedLines != null && syncedLines[i].lyric.speaker == SpeakerEntity.Voice2)
+			val align = if (prefs.getBooleanStrict("lyric_center", false))
+				Layout.Alignment.ALIGN_CENTER
+			else if (syncedLines != null && syncedLines[i].lyric.speaker == SpeakerEntity.Voice2)
 				Layout.Alignment.ALIGN_OPPOSITE
 			else Layout.Alignment.ALIGN_NORMAL
 			val tl = syncedLines != null && syncedLines[i].isTranslated
 			val bg = syncedLines != null && syncedLines[i].lyric.speaker == SpeakerEntity.Background
-			Pair(StaticLayoutBuilderCompat.obtain(sb,
+			val paddingTop = if (syncedLines?.get(i)?.isTranslated == true) 2 else 18
+			val paddingBottom = if (i + 1 < (syncedLines?.size ?: -1) &&
+				syncedLines?.get(i + 1)?.isTranslated == true) 2 else 18
+			SbItem(StaticLayoutBuilderCompat.obtain(sb,
 				if (tl && bg) translationBackgroundTextPaint else if (tl || bg)
-					translationTextPaint else defaultTextPaint, (width * 0.8f).toInt())
+					translationTextPaint else defaultTextPaint, (width * smallSizeFactor).toInt())
 				.setAlignment(align)
-				.build(), sb)
+				.build(), sb, paddingTop.dpToPx(context), paddingBottom.dpToPx(context))
 		}
-		val heights = spLines.map { it.first.height }
-		return Pair(Pair(width, (heights.max() * 0.25f + heights.sum()).toInt()), spLines)
+		val heights = spLines.map { it.layout.height + it.paddingTop + it.paddingBottom }
+		return Pair(Pair(width, (heights.max() / smallSizeFactor + heights.sum()).toInt()), spLines)
 	}
+
+	data class SbItem(val layout: StaticLayout, val text: SpannableStringBuilder, val paddingTop: Int, val paddingBottom: Int)
 }
